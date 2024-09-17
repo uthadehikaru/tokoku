8aae65c7cd3ff05ddfa191955748632bc8a75d13
zuhri utama <zuhriutama@gmail.com>
2024-09-17 09:28:02 +0700

    change log viewer

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,3 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::view('/', 'welcome');
-
-Route::middleware('auth')->group(function() {
-    Route::get('logs', [\Rap2hpoutre\LaravelLogViewer\LogViewerController::class, 'index']);
-});
